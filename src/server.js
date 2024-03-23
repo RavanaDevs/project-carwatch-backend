755e156418b2644e321d74ab05934759f7d96de3
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-23 21:54:15 +0530

    Basic server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,36 @@
-import express from "express";
+import express from 'express'
+import { WebSocketServer } from 'ws'
+import http from 'http'
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res, next) => {
-  return res.send("CarWatch");
-});
+const server = http.createServer(app)
 
-app.listen(5000, () => {
-  console.log("server is listning on port 5000");
-});
+const wss = new WebSocketServer({
+  server: server,
+  path: '/ws',
+})
+
+wss.on('connection', (socket) => {
+  console.log('Clent Connected')
+
+  socket.on('message', async (message) => {
+    try {
+      const jsonData = JSON.parse(message)
+      console.log(jsonData)
+      await handleDeviceInput(jsonData)
+      socket.send('200')
+    } catch (error) {
+      socket.send('Error')
+      console.log(error)
+    }
+  })
+})
+
+app.get('/', (req, res, next) => {
+  return res.send('CarWatch')
+})
+
+server.listen(5000, () => {
+  console.log('server is listning on port 5000')
+})

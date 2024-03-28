a7af0048641f16475a8e40a08636ec56ed714ad2
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-28 19:23:37 +0530

    Decode RPM

--- src/websocket.js
+++ src/websocket.js
@@ -19,12 +19,28 @@ wss.on('connection', (socket) => {
   console.log('Clent Connected')
   socket.on('message', (message) => {
     const jsonMessage = JSON.parse(message.toString())
-    io.emit('debug-msg', jsonMessage)
+    Object.entries(jsonMessage).forEach(([key, value]) => {
+      decodeMessage(key, value.trim())
+    })
   })
 })
 
 io.on('connection', async (socket) => {
   console.log('Client Connected', socket.id)
 })
+
+const decodeMessage = (key, value) => {
+  const msg = {}
+  msg[key] = value
+
+  if (key === '010c') {
+    const a = Number('0x' + value.substring(6, 8))
+    const b = Number('0x' + value.substring(9))
+    const rpm = (256 * a + b) / 4
+    console.log('rpm', rpm)
+    msg[key] = rpm
+  }
+  io.emit('debug-msg', msg)
+}
 
 export default server

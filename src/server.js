891ec0589d2a786487618bdb7392b0ad90a7ebcd
Kavindu Sanjula <ravanadevs@gmail.com>
2024-04-30 12:42:58 +0530

    Setup Mongodb

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
+import 'dotenv/config.js'
 import server from './websocket.js'
+import mongoose from 'mongoose'
 
-server.listen(5000, () => {
-  console.log('server is listning on port 5000')
+const CONN_STR = process.env.MONGODB_CONN_STRING
+const PORT = process.env.PORT
+
+mongoose.connect(CONN_STR).then(() => {
+  console.log('Database Connected')
+  server.listen(PORT, () => {
+    console.log('server is listning on port', PORT)
+  })
 })

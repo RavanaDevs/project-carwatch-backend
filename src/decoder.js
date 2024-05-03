94e6240d63253f8a84779cdc96842c3d0e9d122d
Kavindu Sanjula <ravanadevs@gmail.com>
2024-05-03 13:39:28 +0530

    Decoder and message controller

---
+++ src/decoder.js
@@ -0,0 +1,60 @@
+const input1 = '010c\r41 0C 00 00 \r\r'
+const input2 = '0111\r41 11 29 \r\r'
+const input3 = '0105\r41 05 68 \r\r'
+const input4 = '010c\rNO DATA\r\r'
+
+//RPM
+const decodeRPM = (bytes) => {
+  const A = Number('0x' + bytes[2])
+  const B = Number('0x' + bytes[3])
+  const rpm = (256 * A + B) / 4
+  console.log('RPM: ', rpm)
+
+  return { rpm: rpm }
+}
+
+//Throttle possition
+const decodeTP = (bytes) => {
+  const A = Number('0x' + bytes[2])
+  const tp = (100 / 255) * A
+  console.log('Throttle Position: ', tp)
+
+  return { tp: tp }
+}
+
+//Engine coolant temperature
+const decodeECT = (bytes) => {
+  const A = Number(bytes[2])
+  //   const A = Number('0x' + bytes[2])
+  const temp = A - 40
+  console.log('Engine coolant temperature: ', temp)
+
+  return { ect: temp }
+}
+
+export const decode = (data) => {
+  const elements = data.trim().split('\r')
+  const bytes = elements[1].trim().split(' ')
+
+  if (elements[1] == 'NO DATA') {
+    console.log('NO DATA')
+    return
+  }
+
+  switch (elements[0]) {
+    case '010c':
+      return decodeRPM(bytes)
+    case '0111':
+      return decodeTP(bytes)
+    case '0105':
+      return decodeECT(bytes)
+    default:
+      console.log('no match')
+      break
+  }
+}
+
+// decode(input1)
+// decode(input2)
+// decode(input3)
+// decode(input4)
---
+++ src/message-controller.js
@@ -0,0 +1,30 @@
+import { setValue } from './cache_db.js'
+import { decode } from './decoder.js'
+import { io } from './websocket.js'
+
+export const backendMsgController = (msg) => {
+  const data = decode(msg['data'])
+  if(!data) return
+  Object.entries(data).forEach(([key, value]) => {
+    // setValue(key,value)
+    frontendMsgController(key, value)
+  })
+}
+
+const frontendMsgController = (key, value) => {
+  io.emit(key, value)
+}
+
+// const decodeMessage = (key, value) => {
+//   const msg = {}
+//   msg[key] = value
+
+//   if (key === '010c') {
+//     const a = Number('0x' + value.substring(6, 8))
+//     const b = Number('0x' + value.substring(9))
+//     const rpm = (256 * a + b) / 4
+//     console.log('rpm', rpm)
+//     msg[key] = rpm
+//   }
+//   io.emit('debug-msg', msg)
+// }

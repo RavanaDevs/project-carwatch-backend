43e6af5e1f5f5a4211fff5675ca62372b64cb399
Kavindu Sanjula <ravanadevs@gmail.com>
2024-05-11 15:33:06 +0530

    Change message controller for gps data

--- src/message-controller.js
+++ src/message-controller.js
@@ -3,13 +3,27 @@ import { decode } from './decoder.js'
 import { io } from './websocket.js'
 
 export const backendMsgController = (msg) => {
-  console.log(msg)
-  const data = decode(msg['data'])
-  if(!data) return
-  Object.entries(data).forEach(([key, value]) => {
-    // setValue(key,value)
-    frontendMsgController(key, value)
-  })
+  if (!msg) return
+
+  if (msg.data) {
+    // console.log(msg.data)
+    const data = decode(msg['data'])
+    Object.entries(data).forEach(([key, value]) => {
+      frontendMsgController(key, value)
+    })
+  }
+
+  if (msg.gps) {
+    console.log(msg.gps)
+    const coords = msg.gps.split(',')
+    frontendMsgController('gps',coords)
+  }
+  // if (msg.data) {
+  //   const data = decode(msg['data'])
+  //   Object.entries(data).forEach(([key, value]) => {
+  //     frontendMsgController(key, value)
+  //   })
+  // }
 }
 
 const frontendMsgController = (key, value) => {
